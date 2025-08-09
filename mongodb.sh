617003682bd9b0fc3d3d49b83533bb5bbb4b23b3
Kiran <kirankm95@gmail.com>
2025-08-09 16:54:32 +0530

    roboshop shell script

---
+++ mongodb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+ID=$(id -u)
+TIME=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIME.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+echo -e "this script started executing at ${TIME}" &>> $LOGFILE
+
+validate(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is $G success $N"
+    else
+        echo -e "$2 is $R failed $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+    then
+        echo -e "$Y you are not root user, please re-try with root user $N"
+        exit 1
+    else
+        echo -e "$G you are root user, hence proceeding $N"
+fi
+        
+
+
+dnf list installed mongodb-org &>> $LOGFILE
+if [ $? -eq 0 ]
+then
+    echo -e "$Y mentioned $package already installed $N"
+else
+    dnf install $package -y &>> $LOGFILE
+    validate $? $package
+fi
+
+systemctl enable mongod &>> $LOGFILE
+validate $? "enabling mongodb"
+
+systemctl start mongod &>> $LOGFILE
+validate $? "starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+validate $? "allowing remote access"
+
+systemctl restart mongod &>> $LOGFILE
+validate $? "re-starting mongodb"
+
